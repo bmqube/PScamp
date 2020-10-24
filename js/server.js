2810741a675083f23cd289e7fc134a14565dea10
bmqube <bmqube1@gmail.com>
2020-10-25 00:00:46 +0600

    Server Changed

--- js/server.js
+++ js/server.js
@@ -1,2 +1,3 @@
-const url = "https://ojscraper.herokuapp.com/";
+// const url = "https://ojscraper.herokuapp.com/";
+const url = "https://ojscraper.azurewebsites.net/";
 // const url = "http://127.0.0.1:5000/";
